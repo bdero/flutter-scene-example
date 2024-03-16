f31790cfa53b0c93bdbf10d2f04885cdaccd8987
Brandon DeRosier <bdero@google.com>
2024-03-16 02:10:23 -0700

    HUD and coin count

--- lib/demo/game.dart
+++ lib/demo/game.dart
@@ -1,4 +1,5 @@
 import 'dart:math' as math;
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
@@ -9,7 +10,7 @@ import 'package:scene_demo/demo/camera.dart';
 import 'package:scene_demo/demo/coin.dart';
 import 'package:scene_demo/demo/player.dart';
 import 'package:scene_demo/demo/player_controller.dart';
-import 'package:vector_math/vector_math_64.dart';
+import 'package:vector_math/vector_math_64.dart' as vm;
 
 class GameWidget extends StatefulWidget {
   const GameWidget({super.key});
@@ -18,31 +19,92 @@ class GameWidget extends StatefulWidget {
   State<GameWidget> createState() => _GameWidgetState();
 }
 
-class CoinCollection {
+class CoinController {
   final List<Coin> coins = [
-    Coin(Vector3(-1.4 - 0.8 * 0, 1.5, -6 - 2 * 0)),
-    Coin(Vector3(-1.4 - 0.8 * 1, 1.5, -6 - 2 * 1)),
-    Coin(Vector3(-1.4 - 0.8 * 2, 1.5, -6 - 2 * 2)),
-    Coin(Vector3(-1.4 - 0.8 * 3, 1.5, -6 - 2 * 3)),
+    Coin(vm.Vector3(-1.4 - 0.8 * 0, 1.5, -6 - 2 * 0)),
+    Coin(vm.Vector3(-1.4 - 0.8 * 1, 1.5, -6 - 2 * 1)),
+    Coin(vm.Vector3(-1.4 - 0.8 * 2, 1.5, -6 - 2 * 2)),
+    Coin(vm.Vector3(-1.4 - 0.8 * 3, 1.5, -6 - 2 * 3)),
     //
-    Coin(Vector3(-15 + 2 * 0, 1.5, 0.5 - 1.2 * 0)),
-    Coin(Vector3(-15 + 2 * 1, 1.5, 0.5 - 1.2 * 1)),
-    Coin(Vector3(-15 + 2 * 2, 1.5, 0.5 - 1.2 * 2)),
-    Coin(Vector3(-15 + 2 * 3, 1.5, 0.5 - 1.2 * 3)),
+    Coin(vm.Vector3(-15 + 2 * 0, 1.5, 0.5 - 1.2 * 0)),
+    Coin(vm.Vector3(-15 + 2 * 1, 1.5, 0.5 - 1.2 * 1)),
+    Coin(vm.Vector3(-15 + 2 * 2, 1.5, 0.5 - 1.2 * 2)),
+    Coin(vm.Vector3(-15 + 2 * 3, 1.5, 0.5 - 1.2 * 3)),
     //
-    Coin(Vector3(7 + 2 * 0, 1.5, -16 + 1.3 * 0)),
-    Coin(Vector3(7 + 2 * 1, 1.5, -16.5 + 1.3 * 1)),
-    Coin(Vector3(7 + 2 * 2, 1.5, -16.5 + 1.3 * 2)),
-    Coin(Vector3(7 + 2 * 3, 1.5, -16 + 1.3 * 3)),
+    Coin(vm.Vector3(7 + 2 * 0, 1.5, -16 + 1.3 * 0)),
+    Coin(vm.Vector3(7 + 2 * 1, 1.5, -16.5 + 1.3 * 1)),
+    Coin(vm.Vector3(7 + 2 * 2, 1.5, -16.5 + 1.3 * 2)),
+    Coin(vm.Vector3(7 + 2 * 3, 1.5, -16 + 1.3 * 3)),
   ];
 
-  Node update(Vector3 playerPosition, double deltaSeconds) {
+  Node update(vm.Vector3 playerPosition, double deltaSeconds) {
     return Node(
       // TODO(bdero): Can this be made more efficient?
       children: coins
           .map((coin) => coin.update(playerPosition, deltaSeconds))
           .whereType<Node>()
           .toList(growable: false),
+    );
+  }
+}
+
+class HUDBox extends StatelessWidget {
+  const HUDBox({super.key, required this.child});
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 18, sigmaY: 18),
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          color: Colors.white.withOpacity(0.1),
+          child: DefaultTextStyle.merge(
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+              shadows: [
+                Shadow(
+                  blurRadius: 4,
+                  color: Colors.black,
+                  offset: Offset(2, 2),
+                ),
+              ],
+            ),
+            child: child,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HUDLabelText extends StatelessWidget {
+  const HUDLabelText({super.key, required this.label, required this.value});
+  final String label;
+  final String value;
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      softWrap: false,
+      overflow: TextOverflow.clip,
+      text: TextSpan(
+        style: DefaultTextStyle.of(context).style,
+        text: label,
+        children: [
+          TextSpan(
+            text: value,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontFamily: "monospace",
+              fontFamilyFallback: ["Courier"],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -55,7 +117,7 @@ class _GameWidgetState extends State<GameWidget> {
   final PlayerController playerController = PlayerController();
   final KinematicPlayer player = KinematicPlayer();
   final FollowCamera camera = FollowCamera();
-  final CoinCollection coins = CoinCollection();
+  final CoinController coins = CoinController();
 
   @override
   void initState() {
@@ -82,25 +144,38 @@ class _GameWidgetState extends State<GameWidget> {
     playerController.updatePlayer(player);
     player.update(deltaSeconds);
 
-    return playerController.getControlWidget(
-      context,
-      SceneBox(
-        root: Node(children: [
-          Node.asset("models/ground.glb"),
-          player.node,
-          Node.transform(
-            transform:
-                Matrix4.translation(camera.position) * Matrix4.rotationY(3),
-            children: [Node.asset("models/sky_sphere.glb")],
+    return Stack(
+      children: [
+        playerController.getControlWidget(
+          context,
+          SceneBox(
+            root: Node(children: [
+              Node.asset("models/ground.glb"),
+              player.node,
+              Node.transform(
+                transform:
+                    Matrix4.translation(camera.position) * Matrix4.rotationY(3),
+                children: [Node.asset("models/sky_sphere.glb")],
+              ),
+              coins.update(player.position, deltaSeconds),
+            ]),
+            camera: camera.update(
+                player.position,
+                vm.Vector3(player.velocityXZ.x, 0, player.velocityXZ.y) *
+                    player.kMaxSpeed,
+                deltaSeconds),
           ),
-          coins.update(player.position, deltaSeconds),
-        ]),
-        camera: camera.update(
-            player.position,
-            Vector3(player.velocityXZ.x, 0, player.velocityXZ.y) *
-                player.kMaxSpeed,
-            deltaSeconds),
-      ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(20),
+          child: HUDBox(
+            child: HUDLabelText(
+              label: "Coins: ",
+              value: "${coins.coins.where((coin) => coin.collected).length}",
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
